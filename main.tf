53534be05b3576e7398aeecb8831dc7468980417
Paul Thrasher <thrashr888@gmail.com>
2019-07-01 16:00:53 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 resource "null_resource" "foo" {
-  triggers {
-    always = "#{uuid()}"
+  triggers = {
+    always = uuid()
   }
 }
 
 resource "random_id" "random" {
-  keepers {
-    uuid = "${uuid()}"
+  keepers = {
+    uuid = uuid()
   }
 
   byte_length = 12
 }
 
 output "random" {
-  value = "${random_id.random.hex}"
+  value = random_id.random.hex
 }
